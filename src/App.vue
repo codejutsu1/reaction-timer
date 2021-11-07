b388a43aced49512e893ac449470f4a6cfab8b76
codejutsu1 <codejutsu@protonmail.com>
2021-11-07 16:11:20 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">
+    Play
+  </button>
+  
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>     
+  <Results :score="score" v-if="showResults" />                                  
+</template>
+
+<script>
+import Block from './components/Block'
+import Results from './components/Results'
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+      score: null
+    }
+  },
+
+  methods: {
+    start() {
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+      this.showResults = false  
+    },
+    endGame(reactionTime) {
+      this.isPlaying = false
+      this.score = reactionTime
+      this.showResults = true
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0;
+}
+button {
+  padding: 10px 20px;
+  letter-spacing: 1px;
+  border-radius: 4px;
+  font-size: 16px;
+  border: 1px solid transparent;
+  cursor: pointer;
+  color: white;
+  background-color: #0faf87;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor:  not-allowed;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p>Reaction Time: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data () {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score == 1){
+            this.rank = "You are a god"
+        }else if(this.score < 11){
+            this.rank = 'You are a demigod'
+        }else if(this.score < 200){
+            this.rank = 'You are a Legend'
+        }else if(this.score < 250){
+            this.rank = "Master Reflexes"
+        }else if(this.score < 400){
+            this.rank = "Rapid Reflexes"
+        }else{
+            this.rank = 'Snail pace...'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
